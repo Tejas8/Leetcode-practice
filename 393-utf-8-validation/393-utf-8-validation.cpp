8565bc37402b45cee644a3ce20433399ef873cac
Tejas8 <31968248+Tejas8@users.noreply.github.com>
2022-09-13 22:50:35 +0530

    Time: 33 ms (17.95%), Space: 14 MB (38.65%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     bool validUtf8(vector<int>& data) {
-        int cnt=0;
+        int cnt=0,nbyte,byte,temp,k;
         int n=data.size();
         while(cnt<n){
-        int nbyte=0;
-        int temp=data[cnt];
+         nbyte=0;
+         temp=data[cnt];
         for(int i=7;i>=0;i--){
-            int k=1;
+         k=1;
             k<<=i;
             if(temp&k)nbyte++;
             else break;
         }
-        cout<<nbyte<<endl;
         if(nbyte==0)nbyte=1;
         else if(nbyte==1||nbyte+cnt>(int)data.size()||nbyte>4) return false; 
             cnt+=1;
             int p=cnt-1;
         for(int i=cnt;i<=p+nbyte-1;i++){
             temp=data[i];
-            int byte=0;
+             byte=0;
             for(int j=7;j>=0;j--){
-            int k=1;
+             k=1;
             k<<=j;
             if(temp&k)byte++;
             else break;
                 cnt++;
         }
-            cout<<byte<<endl;
             if(byte!=1) return false;
         }
         }
