5ff1b3971f61a320dbf2b0e5b656969380e9d0da
Tejas8 <31968248+Tejas8@users.noreply.github.com>
2023-08-08 13:13:57 +0530

    Time: 214 ms (81.59%), Space: 47 MB (61.54%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     unordered_set<string>st;
     int ans=0;
-    void fun(string s,int i){
-        if(i>=s.size()){
+    void fun(string s){
+        if(1>=s.size()){
             ans=max(ans,(int)st.size());
             return;
         }
         
-        for(int j=i;j<s.size();j++){
+        for(int j=1;j<s.size();j++){
         string temp=s.substr(0,j);
         if(st.find(temp)!=st.end()) continue;
         else{
@@ -16,7 +16,7 @@ public:
             st.insert(s.substr(j));
             ans=max(ans,(int)st.size());
             st.erase(s.substr(j));
-            fun(s.substr(j),1);
+            fun(s.substr(j));
             st.erase(temp);
         }    
         }
@@ -25,8 +25,8 @@ public:
     int maxUniqueSplit(string s) {
         int n=s.size();
         
-        int i=1;
-        fun(s,i);
+      //  int i=1;
+        fun(s);
         return ans;
     }
 };
